82488f728d708d5946c8f12f61a0dd0e12f46b43
LJ3D <65134690+LJ3D@users.noreply.github.com>
2022-06-14 11:40:18 +0100

    js test time

---
+++ KelhamARPages/TestARPage/test.js
@@ -0,0 +1,22 @@
+const modelViewer = document.getElementsByTagName('model-viewer')[0];
+
+function GetURLParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+}
+
+function configModelViewer(){
+    var model = GetURLParameter('model');
+    alert(model)
+    modelViewer.src = model;
+}
+
